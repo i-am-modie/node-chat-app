3d6fb628224fa163e7d3a38c7b6a925e07b25458
Jan Dubniak <narusteras@gmail.com>
2018-03-26 23:36:05 +0200

    welcome message

--- server/server.js
+++ server/server.js
@@ -16,11 +16,23 @@ app.use(express.static(publicPath));
 io.on('connection', (socket)=>{
    console.log('New user connected');
 
+   socket.emit('newMessage', {
+       from: 'Admin',
+       text: 'Welcome to the soldier side',
+       CreatedAt: new Date().getTime()
+   });
+
+   socket.broadcast.emit('newMessage', {
+       from: 'Admin',
+       text: 'Someone joined will he say "Hi" or it\' ordinary creep?',
+       CreatedAt: new Date().getTime()
+   });
+
    socket.on('createMessage',(newMessage)=>{
        console.log(`Message from ${newMessage.from}`, newMessage);
        let message = _.pick(newMessage, ['from', 'text']);
        message.CreatedAt = new Date().getTime();
-       io.emit('newMessage', message);
+       socket.broadcast.emit('newMessage', message);
    });
 
    socket.on('disconnect', ()=>{

4ca283e66b26ec2e3464aab305572f4d766a9724
Jan Dubniak <narusteras@gmail.com>
2018-04-01 19:22:21 +0200

    + scrollToBottom if client is near last message

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,21 @@
 var socket = io();
 
+function scrollToBottom() {
+    //Selectors
+    var messages = $('#messages');
+    var newMessage = messages.children('li:last-child');
+    //Heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+}
+
 socket.on('connect', function (){
     console.log('Connected to server');
 
@@ -14,6 +30,7 @@ socket.on('newMessage', function (message) {
         createdAt: formatedTime,
     });
     $('#messages').append(html);
+    scrollToBottom();
 });
 
 socket.on('newLocationMessage', function (message) {
@@ -25,6 +42,7 @@ socket.on('newLocationMessage', function (message) {
         createdAt: formatedTime,
     });
     $('#messages').append(html);
+    scrollToBottom();
 });
 
 socket.on('disconnect', function (){
